495fbbb95733f4cd09ba5add7b299ea7fb8a15bf
neiloy <neiloyhassan1@gmail.com>
2026-03-12 15:56:04 +0600

    fixed

--- app/dashboard/qp-payment/page.tsx
+++ app/dashboard/qp-payment/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import QpPaymentPageComponent from "@/components/pages/qp/qp-payment";
 
-export default function QpPaymentPage() {
+function QpPaymentContent() {
   const searchParams = useSearchParams();
   const chargeFileId = searchParams.get("chargeFileId") ?? undefined;
   return <QpPaymentPageComponent initialChargeFileId={chargeFileId} />;
+}
+
+export default function QpPaymentPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <QpPaymentContent />
+    </Suspense>
+  );
 }
